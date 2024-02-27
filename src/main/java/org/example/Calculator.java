72a98fb968df372e88d168567642684ab09c1a7f
solarqk <koehlecarson@gmail.com>
2024-02-26 22:15:22 -0700

    running tests on IDE

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,84 @@
+package org.example;
+
+public class Calculator { //this is a comment
+
+    public Calculator(){
+
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+
+
+    /*
+    Returns the n'th number in the fibonacci sequence
+    https://en.wikipedia.org/wiki/Fibonacci_number
+    Example below
+    n = x
+    1 = 1
+    2 = 1
+    3 = 2
+    4 = 3
+    5 = 5
+    .
+    .
+    .
+    etc
+     */
+    public int fibonacciNumberFinder(int n){
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        return b;
+    }
+
+
+    /*
+    Returns binary value of the given int number
+    The binary number will be stored as a string
+    if int a = 0 then this method returns: 0
+    if int a = 10 then this method returns: 1010
+    if int a = 16 then this method returns: 10000
+     */
+    public String intToBinaryNumber(int number){
+        return Integer.toBinaryString(number);
+    }
+
+    /*
+    Create a completely unique String identifier for a given string
+    Each createdID must contain the string n in its unaltered Form
+    if String n = "Jason"
+    then the created ID could be = Jasonklfgn3jknnvksdfm - Because it contains the unaltered String n and is unique
+
+    if you run this function twice with the same String input, it must return 2 unique String IDs
+     */
+    public String createUniqueID(String n){
+        long currentTimeMillis = System.currentTimeMillis();
+        long randomNum = (long) (Math.random() * Long.MAX_VALUE);
+        return n + currentTimeMillis + "-" + randomNum;
+    }
+
+
+
+}
