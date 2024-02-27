a9135b5bb2a7d5e02220f9a63c73d999dcae8490
solarqk <koehlecarson@gmail.com>
2024-02-26 22:51:42 -0700

    Trying it again with hopefully correct Docker credentials(1)

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator { //this is a comment
 
     public int subtract(int a, int b){
         return a - b;
-    }// comment
+    }// comment 2
 
     public int multiply(int a, int b){
         return a * b;
